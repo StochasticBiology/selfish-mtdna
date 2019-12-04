ef462d3a050ce8674171ed54be4fc9c572e73eed
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2019-12-04 13:03:31 +0000

    Add files via upload

---
+++ sim/run.sh
@@ -0,0 +1,8 @@
+# wrapper script to compile and run mtDNA simulations: RTS with mito- and cell-level selection
+
+gcc -o3 mtdna-mito.c -lm -o mtdna-mito.ce
+gcc -o3 mtdna-cell.c -lm -o mtdna-cell.ce
+
+./mtdna-mito.ce 0 > tmp0 &
+./mtdna-mito.ce 1 > tmp1 &
+./mtdna-cell.ce > tmp2 &
