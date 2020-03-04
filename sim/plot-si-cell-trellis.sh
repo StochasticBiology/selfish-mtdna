4efe3516fd1b3f6e1d58173d7c159eebdc89f3ea
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-03-04 12:12:59 +0100

    Add files via upload
    Update plotting scripts for simulation output

--- sim/plot-si-cell-trellis.sh
+++ sim/plot-si-cell-trellis.sh
@@ -1,36 +1,76 @@
-# Gnuplot: plot trellis of cell-level RTS behaviour for different parameterisations
-# Suggested terminal: png 1200 800
+# Gnuplot: plot trellis of RTS behaviour with different lambda1 values
+# suggested terminal: png 1600 1000
 
 reset
 set multiplot
-set size 0.33,0.5
-r = 0.2
-set cbrange [-r:r]
-set palette defined (-r "#AAAAFF", 0 "white", r "#FFAAAA")
-set pm3d map corners2color c1
-set xlabel "Relative foreign\nselfishness"
-set ylabel "Cell-level selectivity"
+set size 0.33
+
 unset key
-set xtics 0.1
+
+r = 1
+set cbrange [-r:r]
+set palette defined (-r "#AAAAFF", 0 "#FFFFFF", r "#FFAAAA")
+set pm3d map corners2color c1
+set ylabel "Cell selective threshold P_C"
+set xlabel "Relative foreign selfishness (λ₂ - λ₁)"
+offset = 0.025
+
+set yrange [0:480]
+set object rectangle from graph 0,0 to graph 1,1 behind fillcolor rgbcolor "#FFFFDD"
 set origin 0,0
+lambda1 = 0.1
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.1" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.100.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
 
-set origin 0,0.5
+set origin 0.33,0
+lambda1 = 0.2
+set label 2 at graph 0.92,graph 0.96 "*" front
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.2" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.200.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
 
-set label 1 at graph 0.05, graph 0.9 "λ1 = 0.2" front
-splot "mtdna-cell-1-10-0.000-0.2.txt" u ($1-0.2):2:($3 == -1 ? 1/0 : $4-$3)
-set origin 0.33,0.5
-set label 1 at graph 0.05, graph 0.9 "λ1 = 0.5" front
-splot "mtdna-cell-1-10-0.000-0.5.txt" u ($1-0.5):2:($3 == -1 ? 1/0 : $4-$3)
-set origin 0.66,0.5
-set label 1 at graph 0.05, graph 0.9 "λ1 = 0.8" front
-splot "mtdna-cell-1-10-0.000-0.8.txt" u ($1-0.8):2:($3 == -1 ? 1/0 : $4-$3)
+set origin 0.66,0
+lambda1 = 0.3
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.3" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.300.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
 
-set origin 0,0.
-set label 1 at graph 0.05, graph 0.9 "λ1 = 0.2" front
-splot "mtdna-cell-0-10-0.000-0.2.txt" u ($1-0.2):2:($3 == -1 ? 1/0 : $4-$3)
-set origin 0.33,0.
-set label 1 at graph 0.05, graph 0.9 "λ1 = 0.5" front
-splot "mtdna-cell-0-10-0.000-0.5.txt" u ($1-0.5):2:($3 == -1 ? 1/0 : $4-$3)
-set origin 0.66,0.
-set label 1 at graph 0.05, graph 0.9 "λ1 = 0.8" front
-splot "mtdna-cell-0-10-0.000-0.8.txt" u ($1-0.8):2:($3 == -1 ? 1/0 : $4-$3)
+
+set origin 0,0.33
+lambda1 = 0.4
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.4" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.400.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
+
+set origin 0.33,0.33
+lambda1 = 0.5
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.5" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.500.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
+
+set origin 0.66,0.33
+lambda1 = 0.6
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.6" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.600.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
+
+
+
+set origin 0,0.66
+lambda1 = 0.7
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.7" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.700.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
+
+set origin 0.33,0.66
+lambda1 = 0.8
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.8" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.800.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
+
+set origin 0.66,0.66
+lambda1 = 0.9
+set label 1 at graph 0.05, graph 0.9 "λ₁ = 0.9" front
+set xrange [-lambda1-offset:0.9-lambda1-offset]
+splot "mtdna-mc-overall-0.900.txt" u ($1-lambda1-offset):2:(2*($3-0.5))
