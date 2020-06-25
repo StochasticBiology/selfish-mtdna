0b8c02e8cff2657f9682b817f745ebdafc136dcc
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-06-25 06:55:13 +0200

    Add files via upload
    Updated to include sims for protein diffusion and replication failure

--- sim/run.sh
+++ sim/run.sh
@@ -3,6 +3,19 @@
 gcc -o3 mtdna-mito.c -lm -o mtdna-mito.ce
 gcc -o3 mtdna-cell.c -lm -o mtdna-cell.ce
 
-./mtdna-mito.ce 0 > tmp0 &
-./mtdna-mito.ce 1 > tmp1 &
+# default mito-level 
+./mtdna-mito.ce 0 0 0 > tmp000 &
+
+# default mito-level zoomed in
+./mtdna-mito.ce 1 0 0 > tmp100 &
+
+# different protein mixing
+./mtdna-mito.ce 0 0 0.01 > tmp001 &
+./mtdna-mito.ce 0 0 0.1 > tmp002 &
+
+# different replication failure rates
+./mtdna-mito.ce 0 0.2 0 > tmp010 &
+./mtdna-mito.ce 0 0.5 0 > tmp020 &
+
+# cell-level
 ./mtdna-cell.ce 0 > tmp2 &
